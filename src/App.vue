de36179bd7f116431731da28691631217304454d
Masaya Kazama <miyan@t-p.jp>
2017-11-08 15:09:05 +0900

    Add style for sp

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,7 @@ export default {
 </script>
 
 <style>
+body {
+  font-family: -apple-system-body, BlinkMacSystemFont, "Helvetica Neue", "Hiragino Sans", "Hiragino Kaku Gothic ProN", "Noto Sans Japanese", "游ゴシック  Medium", "Yu Gothic Medium", "メイリオ", meiryo, sans-serif;
+}
 </style>
